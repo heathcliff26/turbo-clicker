47ec0e3e7d6b02761bb6fef12fbbe10d5e8df12b
Heathcliff <heathcliff@heathcliff.eu>
2025-07-27 20:30:31 +0200

    Move autoclicker implementation to its own file
    Refactor the code to make autoclicker into a separate struct.
    This improves code readability and maintainability.
    Additionally it decuples the gui from the backend logic.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- src/main.rs
+++ src/main.rs
@@ -2,70 +2,35 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
 
-use evdev::uinput::VirtualDevice;
-use evdev::{AttributeSet, KeyCode, KeyEvent};
+mod autoclicker;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut autoclicker = autoclicker::Autoclicker::new()?;
     let ui = AppWindow::new()?;
 
-    ui.on_start_auto_click(start_auto_clicker);
+    ui.on_start_auto_click(
+        move |delay: i32,
+              start_delay: i32,
+              duration: i32,
+              use_start_delay: bool,
+              use_duration: bool| {
+            let start_delay: Option<u64> = match use_start_delay {
+                true => Some(start_delay.try_into().unwrap()),
+                false => None,
+            };
+            let duration: Option<u64> = match use_duration {
+                true => Some(duration.try_into().unwrap()),
+                false => None,
+            };
+
+            autoclicker.autoclick(delay.try_into().unwrap(), start_delay, duration);
+        },
+    );
 
     ui.run()?;
 
     Ok(())
-}
-
-/// Starts an auto clicker in the background, simulating left mouse clicks with the given delay (ms).
-/// Returns a handle to stop the clicker via the returned `watch::Sender<bool>`.
-fn start_auto_clicker(delay_ms: i32, duration: i32, start_delay: i32) {
-    let stop = Arc::new(Mutex::new(false));
-    let stop_clone = Arc::clone(&stop);
-
-    let mut device = VirtualDevice::builder()
-        .expect("Failed to create virtual device")
-        .name("turbo-clicker-mouse")
-        .with_keys(&AttributeSet::from_iter([KeyCode::BTN_LEFT]))
-        .expect("Failed to add BTN_LEFT key")
-        .build()
-        .expect("Failed to build virtual device");
-
-    thread::spawn(move || {
-        let stop = Arc::clone(&stop_clone);
-        println!("Waiting for clicker to start");
-        thread::sleep(Duration::from_secs(start_delay.try_into().unwrap()));
-        println!("Starting loop");
-        loop {
-            if *stop.lock().unwrap() {
-                println!("Received stop signal");
-                break;
-            }
-            println!("Clicking...");
-
-            device
-                .emit(&[*KeyEvent::new(KeyCode::BTN_LEFT, 1)])
-                .expect("Failed to emit key press");
-            device
-                .emit(&[*KeyEvent::new(KeyCode::BTN_LEFT, 2)])
-                .expect("Failed to emit key release");
-
-            println!("Clicked");
-            thread::sleep(Duration::from_millis(delay_ms.try_into().unwrap()));
-        }
-    });
-
-    thread::spawn(move || {
-        println!("Waiting for duration to end");
-        thread::sleep(Duration::from_secs(
-            (duration + start_delay).try_into().unwrap(),
-        ));
-        println!("Sending stop signal");
-        let mut stop = stop.lock().unwrap();
-        *stop = true;
-    });
 }
