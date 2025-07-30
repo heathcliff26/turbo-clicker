71098c604211c33b75f019e9ef2c742323ac2ec7
Heathcliff <heathcliff@heathcliff.eu>
2025-07-30 12:57:44 +0200

    Install: Add udev rule for uinput permissions
    Remove flathub installation method. With the needed permissions to access
    `/dev/uinput`, running the app in a flatpak sandbox makes no sense.
    
    Install a udev rule for accessing `/dev/uinput` as part of the installation
    script.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- packages/install-desktop.sh
+++ packages/install-desktop.sh
@@ -4,6 +4,8 @@ base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)"
 
 APP_ID="io.github.heathcliff26.turbo-clicker"
 DESKTOP_FILE_TARGET="${HOME}/.local/share/applications/${APP_ID}.desktop"
+UDEV_RULES_DIR="/usr/lib/udev/rules.d"
+UDEV_RULE="99-turbo-clicker-input.rules"
 
 help() {
     echo "Integrate Turbo Clicker with common desktop environments."
@@ -24,11 +26,19 @@ install() {
     else
         echo "The app will not show up in the menu until the session is restarted"
     fi
+
+    echo "Installing udev rules for Turbo Clicker"
+    sudo cp "${base_dir}/${UDEV_RULE}" "${UDEV_RULES_DIR}/${UDEV_RULE}"
+    sudo udevadm control --reload-rules
+    sudo udevadm trigger
+    echo "It might take a reboot for the udev rules to take effect."
 }
 
 uninstall() {
     rm "${DESKTOP_FILE_TARGET}"
     echo "Removed: ${DESKTOP_FILE_TARGET}"
+    sudo rm "${UDEV_RULES_DIR}/${UDEV_RULE}"
+    echo "Removed udev rules: ${UDEV_RULES_DIR}/${UDEV_RULE}"
 }
 
 while [[ "$#" -gt 0 ]]; do
